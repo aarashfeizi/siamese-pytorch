971d2c0ffe28c8d3ad494d4c05bdf61b93adef83
Aarash Feizi <aarash95@gmail.com>
2020-03-23 03:25:33 -0400

    added epochs

--- train.py
+++ train.py
@@ -73,49 +73,52 @@ if __name__ == '__main__':
 
     print('steps:', args.max_steps)
 
-    for batch_id, (img1, img2, label) in enumerate(trainLoader, 1):
-        if batch_id > args.max_steps:
-            break
-        if args.cuda:
-            img1, img2, label = Variable(img1.cuda()), Variable(img2.cuda()), Variable(label.cuda())
-        else:
-            img1, img2, label = Variable(img1), Variable(img2), Variable(label)
-        opt.zero_grad()
-        output = net.forward(img1, img2)
-        loss = loss_fn(output, label)
-        loss_val += loss.item()
-        loss.backward()
+    epochs = int(args.max_steps / len(trainLoader))
 
-        opt.step()
-        if batch_id % args.log_freq == 0:
-            print('[%d]\tloss:\t%.5f\ttime lapsed:\t%.2f s' % (
-                batch_id, loss_val / args.log_freq, time.time() - time_start))
-            loss_val = 0
-            time_start = time.time()
+    for epoch in range(epochs):
+        for batch_id, (img1, img2, label) in enumerate(trainLoader, 1):
+            if batch_id > args.max_steps:
+                break
+            if args.cuda:
+                img1, img2, label = Variable(img1.cuda()), Variable(img2.cuda()), Variable(label.cuda())
+            else:
+                img1, img2, label = Variable(img1), Variable(img2), Variable(label)
+            opt.zero_grad()
+            output = net.forward(img1, img2)
+            loss = loss_fn(output, label)
+            loss_val += loss.item()
+            loss.backward()
 
-        if batch_id % args.save_freq == 0:
-            torch.save(net.state_dict(), args.save_path + '/model-inter-' + str(batch_id + 1) + ".pt")
+            opt.step()
+            if batch_id % args.log_freq == 0:
+                print('epoch: %d, batch: [%d]\tloss:\t%.5f\ttime lapsed:\t%.2f s' % (
+                    epoch, batch_id, loss_val / args.log_freq, time.time() - time_start))
+                loss_val = 0
+                time_start = time.time()
 
-        if batch_id % args.test_freq == 0:
-            right, error = 0, 0
-            for _, (test1, test2) in enumerate(testLoader, 1):
-                if args.cuda:
-                    test1, test2 = test1.cuda(), test2.cuda()
-                test1, test2 = Variable(test1), Variable(test2)
-                output = net.forward(test1, test2).data.cpu().numpy()
-                pred = np.argmax(output)
-                if pred == 0:
-                    right += 1
-                else:
-                    error += 1
+            if batch_id % args.save_freq == 0:
+                torch.save(net.state_dict(), args.save_path + '/model-inter-' + str(batch_id + 1) + ".pt")
 
-            print('*' * 70)
-            print('[%d]\tTest set\tcorrect:\t%d\terror:\t%d\tprecision:\t%f' % (
-                batch_id, right, error, right * 1.0 / (right + error)))
-            print('*' * 70)
-            queue.append(right * 1.0 / (right + error))
-        train_loss.append(loss_val)
-    #  learning_rate = learning_rate * 0.95
+            if batch_id % args.test_freq == 0:
+                right, error = 0, 0
+                for _, (test1, test2) in enumerate(testLoader, 1):
+                    if args.cuda:
+                        test1, test2 = test1.cuda(), test2.cuda()
+                    test1, test2 = Variable(test1), Variable(test2)
+                    output = net.forward(test1, test2).data.cpu().numpy()
+                    pred = np.argmax(output)
+                    if pred == 0:
+                        right += 1
+                    else:
+                        error += 1
+
+                print('*' * 70)
+                print('epoch: %d, batch: [%d]\tTest set\tcorrect:\t%d\terror:\t%d\tprecision:\t%f' % (
+                    epoch, batch_id, right, error, right * 1.0 / (right + error)))
+                print('*' * 70)
+                queue.append(right * 1.0 / (right + error))
+            train_loss.append(loss_val)
+        #  learning_rate = learning_rate * 0.95
 
     with open('train_loss', 'wb') as f:
         pickle.dump(train_loss, f)
