616452e06f08c95242f39d38c8e4ea31301ab6eb
Aarash Feizi <aarash95@gmail.com>
2020-04-02 21:45:46 -0400

    add stats to csv

--- make_hotel50_data.py
+++ make_hotel50_data.py
@@ -1,11 +1,25 @@
 import argparse
 import os
+import statistics
 
+import numpy as np
 import pandas as pd
+from PIL import Image
 
 
 def _check_dir(path):
     return os.path.isdir(path)
+
+
+def get_stats(l):
+    length = len(l)
+    mean = l.mean()
+    median = statistics.median(l)
+    max = l.max()
+    min = l.min()
+    u_v = len(np.unique(l))
+
+    return {'length': length, 'mean': mean, 'median': median, 'max': max, 'min': min, 'unique_values': u_v}
 
 
 def load_hotels_data(path):
@@ -14,62 +28,84 @@ def load_hotels_data(path):
     image_list = []
     super_class_list = []
 
-    fst_l_d = os.listdir(path)  # e.g. 1 10 11 12
+    if os.path.exists(os.path.join(path, 'image_label.csv')):
 
-    label = 0
-    super_class = 0
+        print('Found csv!')
 
-    for f_dir in fst_l_d:
-        scd_path = os.path.join(path, f_dir)
-        print(scd_path)
+        dataset = pd.read_csv(os.path.join(path, 'hotel50-image_label.csv'))
 
-        if not _check_dir(scd_path):
-            continue
+    else:
+        print('File not found, creating csv...')
 
-        scd_l_d = os.listdir(scd_path)  # e.g. 9645 20303 3291 35913
+        path = os.path.join(path, 'images/train/')
 
-        for s_dir in scd_l_d:  # All same super_class
-            thd_path = os.path.join(scd_path, s_dir)
+        fst_l_d = os.listdir(path)  # e.g. 1 10 11 12
 
-            if not _check_dir(thd_path):
+        label = 0
+        super_class = 0
+
+        for f_dir in fst_l_d:
+            scd_path = os.path.join(path, f_dir)
+            print(scd_path)
+
+            if not _check_dir(scd_path):
                 continue
 
-            thd_l_d = os.listdir(thd_path)  # e.g. traffickcam travel_website
+            scd_l_d = os.listdir(scd_path)  # e.g. 9645 20303 3291 35913
 
-            for t_dir in thd_l_d:  # all same labels
+            for s_dir in scd_l_d:  # All same super_class
+                thd_path = os.path.join(scd_path, s_dir)
 
-                imagedir_path = os.path.join(thd_path, t_dir)
-
-                if not _check_dir(imagedir_path):
+                if not _check_dir(thd_path):
                     continue
 
-                if t_dir == 'travel_website':
-                    is_website = 1
-                elif t_dir == 'traffickcam':
-                    is_website = 0
-                else:
-                    print(imagedir_path)
-                    raise Exception('FUCK')
+                thd_l_d = os.listdir(thd_path)  # e.g. traffickcam travel_website
 
-                if not _check_dir(imagedir_path):
-                    continue
+                for t_dir in thd_l_d:  # all same labels
 
-                images = os.listdir(imagedir_path)  # e.g. *.jpg
+                    imagedir_path = os.path.join(thd_path, t_dir)
 
-                for image in images:
-                    image_path = os.path.join(imagedir_path, image)
-                    image_list.append(image_path[image_path.find('images'):])
-                    hotel_label_list.append(label)
-                    cam_web_list.append(is_website)
-                    super_class_list.append(super_class)
+                    if not _check_dir(imagedir_path):
+                        continue
 
-            label += 1
-        super_class += 1
+                    if t_dir == 'travel_website':
+                        is_website = 1
+                    elif t_dir == 'traffickcam':
+                        is_website = 0
+                    else:
+                        print(imagedir_path)
+                        raise Exception('FUCK')
 
-    dataset = pd.DataFrame({'image': image_list, 'hotel_label': hotel_label_list, 'super_class': super_class_list,
-                           'is_website': cam_web_list})
-    dataset.to_csv('image_label_2.csv', index=False, header=True)
+                    if not _check_dir(imagedir_path):
+                        continue
+
+                    images = os.listdir(imagedir_path)  # e.g. *.jpg
+
+                    for image in images:
+                        image_path = os.path.join(imagedir_path, image)
+                        image_list.append(image_path[image_path.find('images'):])
+                        hotel_label_list.append(label)
+                        cam_web_list.append(is_website)
+                        super_class_list.append(super_class)
+
+                label += 1
+            super_class += 1
+
+        dataset = pd.DataFrame({'image': image_list, 'hotel_label': hotel_label_list, 'super_class': super_class_list,
+                                'is_website': cam_web_list})
+        dataset.to_csv('image_label.csv', index=False, header=True)
+
     return dataset
+
+
+def get_size(df, path):
+    sizes = []
+
+    for idx, row in df.iterrows():
+        img = Image.open(os.path.join(path, row[0]))
+        sizes.append(img.size)
+
+    return sizes
 
 
 def main():
@@ -81,6 +117,13 @@ def main():
 
     df = load_hotels_data(args.path)
 
+    sizes = get_size(df, args.path)
+
+    print('shape 0')
+    print(get_stats(np.array(list(zip(*sizes))[0])))
+    print('*' * 70)
+    print('shape 1')
+    print(get_stats(np.array(list(zip(*sizes))[1])))
 
 if __name__ == '__main__':
     main()
